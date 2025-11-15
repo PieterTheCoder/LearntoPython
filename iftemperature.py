e559d3967aae6443f958763bfc25cadfc5a12ac9
Piter <pirotb1001@gmail.com>
2025-11-15 11:46:15 +0700

    Update iftemperature.py

---
+++ iftemperature.py
@@ -0,0 +1,8 @@
+temperature = input('Enter temperature in Celcius! ')
+if int(temperature) >= 30:
+    print('Its a hot day!')
+    print('Drink some water to hydrate')
+elif int(temperature) > 19.29:
+    print('Its a nice day!')
+elif int(temperature) <= 19:
+    print('Its cold day!')
