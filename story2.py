0644c6fca31c64285a214f0581863030a174988c
Piter <pirotb1001@gmail.com>
2025-12-06 21:46:57 +0700

    Make story2.py

---
+++ story2.py
@@ -0,0 +1,15 @@
+import time
+print("Story")
+time.sleep(1)
+print("\nWelcome to the Story")
+print("""
+You wake up on city called Mixerus... that famous because in this city you can find:
+- Eagle that can beatbox 24 hours
+- Mixer that turns on 24 hours
+- Cat that dances randomly every morning
+
+You go out of your house with a cup of coffee on your hand and breathe in... the smelly air.
+It smells like noodle with tomato sauce and tomato sauce without noodle at the same time.
+
+You realize something is not as usual...
+""")
