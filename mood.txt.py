e2610bb9a09e23fdc74a2fac1ae5b80f6f2770aa
Piter <pirotb1001@gmail.com>
2025-12-13 22:43:43 +0700

    Make mood.txt.py

---
+++ mood.txt.py
@@ -0,0 +1,6 @@
+print('Daily Mood Tracker')
+date = input('Today date: ')
+mood = input('Mood now: ')
+with open ("mood.txt", "a") as file:
+    file.write(date + "| Mood:" + mood + "\n")
+print("Your mood has been saved")
