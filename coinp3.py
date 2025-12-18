9dcc1a9e5e47dd2e96a8ba4c44f09f363cf27494
Piter <pirotb1001@gmail.com>
2025-12-18 22:06:38 +0700

    Update coinp3.py

--- coinp3.py
+++ coinp3.py
@@ -1,6 +1,6 @@
 import random
 print('Mastering...')
-choice = input('Choose [H/T]')
+choice = input('Choose a coin to flip [H/T]')
 result = random.choice(['H', 'T'])
 if choice.upper == result:
    print(f'You Win {result}')
