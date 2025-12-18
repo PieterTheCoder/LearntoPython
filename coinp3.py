64de459faf20b7650bc7ab2d9ba01952008184ce
Piter <pirotb1001@gmail.com>
2025-12-18 22:04:44 +0700

    Update coinp3.py

--- coinp3.py
+++ coinp3.py
@@ -1,3 +1,8 @@
 import random
 print('Mastering...')
-choice = input('Choose [H,T])
+choice = input('Choose [H/T]')
+result = random.choice(['H'/'T'])
+if choice == result:
+   print(f'You Win {result}')
+else:
+   print(f'You Lose {result}')
