fe5331f943a46cb40b980a47a8ca381d7d775184
Piter <pirotb1001@gmail.com>
2025-12-28 14:14:27 +0700

    Make lenp2.py and programmernamegenerator.py

---
+++ lenp2.py
@@ -0,0 +1,4 @@
+print("Your name length is: " + str(len(input("What is your name? \n"))))
+usn = input("What is your favorite usn? \n")
+length = len(usn)
+print("Your name length is: " + str(length))
---
+++ programmernamegenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Programmer name generator")
+color = input("What is your favorite color?")
+country = input("What country you come from?")
+print("Your Programmer name is " + str(color) + " " + str(country))
