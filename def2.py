b65c24140b409a77528691be7bbbf4983ed11dd0
Piter <pirotb1001@gmail.com>
2026-01-05 23:11:37 +0700

    Make def2.py

---
+++ def2.py
@@ -0,0 +1,5 @@
+def greet_with(name, location):
+    print(f"Hello {name}!""")
+    print(f"How is it like in {location}?")
+greet_with("Asta", "Clover Kingdom")
+greet_with(location = "Clover Kingdom", name = "Yuno")
