f4ea31e6c884bf8a9888b875dbe70f514f2531a8
Piter <pirotb1001@gmail.com>
2026-01-16 21:34:51 +0700

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+fruits = {
+"Orange" = "A fruit, the color is green,yellow, and orange itself. It can be sweet or sour."
+"Apple" = "A fruit, the color is red and green. Its taste sweet or sour."
+}
+print(fruits)
+fruits["Apple"] = "A fruit."
+print(fruits)
+for fruit in fruits
+    print(key)
+    print(key[Apple])
