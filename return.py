dfff8c25a93c8e9502e84907a9b480d963c63e69
Piter <pirotb1001@gmail.com>
2026-01-23 22:26:56 +0700

    Make return.py

---
+++ return.py
@@ -0,0 +1,9 @@
+def f_name(text):
+    storage = text + text + text + text
+    return storage
+
+def l_name(text):
+    storage2 = text + text + text + text
+    return storage2
+
+print(l_name(f_name("dog")))
