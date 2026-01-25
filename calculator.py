a982abb2f3d974a5ae8a7b2e59ca26a10c482258
Piter <pirotb1001@gmail.com>
2026-01-25 15:28:03 +0700

    Make calculator.py

---
+++ calculator.py
@@ -0,0 +1,52 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+def calculator():
+    Pilo = True
+    print(r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+""")
+    n3 = float(input("What is the first number?  "))
+    while Pilo:
+        operations1 = str(input("+\n-\n*\n/\n\Pick an operation:? "))
+        n4 = float(input("What is the second number?  "))
+        operations2 = ""
+        operations2 += operations1
+        result = (operations[operations2](n3, n4))
+        print(f"{n3} {operations2} {n4} = {result}")
+        decision = input(f"Type 'y' to continue with {result} or type 'n' with the new calculation ").lower()
+        if decision == "y":
+            num3 = result
+        if decision == "n":
+            print("\n" * 20)
+            calculator()
+
+calculator()
--- calculator_project.py
+++
@@ -1,4 +0,0 @@
-number1 = int(input("What is the first number? "))
-print("+\n-\n*\n/")
-operation = input("What operation do you you want? ")
-number2 = float(input("What is the second number? "))
