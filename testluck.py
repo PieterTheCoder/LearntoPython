76f3fa042f32fadd54f87e24a5136ef7a3617956
Piter <pirotb1001@gmail.com>
2026-01-27 21:03:53 +0700

    Edit testluck.py

--- testluck.py
+++ testluck.py
@@ -1,5 +1,5 @@
 import random
-print("WELCOME TO THE LUCK TEST\nIn here we gonna test from 1-200 if you hit 100/200 that 100 pure luck")
+print("WELCOME TO THE LUCK TEST\nIn here we gonna test from 1-200 if you hit 100/200 that 100% pure luck")
 number = random.choice(range(1, 201))
 print(number)
 if number == 100 and 200:
